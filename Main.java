18cb01d9f6449ed5b922a502b6c2789418662065
stvnu <stevenyuuuu@gmail.com>
2023-08-01 01:13:32 +0800

    MP DONE! FOR REAL

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String[] args){
         VendingMachineChoiceFrame frame = new VendingMachineChoiceFrame();
 
-        frame.setVisible(true);1
+        frame.setVisible(true);
     }
 }
